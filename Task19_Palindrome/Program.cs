d3bfefed66f94256e15fdfb1a5cff50e8e00fc1a
annmark94 <annmarkova94@gmail.com>
2022-07-03 09:43:24 +0300

    Initial commit - added the folder Task19_Palindrome

---
+++ Task19_Palindrome/Program.cs
@@ -0,0 +1,16 @@
+﻿// Find if the number is a Palindrome
+Console.Write("Enter a 5-digit number: ");
+int number = 0;
+while (!int.TryParse(Console.ReadLine(), out number)
+       || number < 10000 || number >= 100000)
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.Write("This is not a 5-digit number, try again: ");
+    Console.ResetColor();
+}
+
+if (number % 10 == number / 10000 &&
+    number % 100 / 10 == number / 1000 % 10 )
+    Console.WriteLine("This number is a Palinedrome! :)");
+else
+    Console.WriteLine("This number is NOT a Palinedrome :(");
