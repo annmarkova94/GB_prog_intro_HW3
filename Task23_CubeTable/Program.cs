87c5844b90ca56fc20cb32ff2c6aa31dd699007a
annmark94 <annmarkova94@gmail.com>
2022-07-03 14:04:51 +0300

    Initial commit for the third folder - added folder Task23_CubeTable

---
+++ Task23_CubeTable/Program.cs
@@ -0,0 +1,29 @@
+﻿// Enter a number N,show Cube table from 1 to N
+CubeTablePrint();
+
+void CubeTablePrint()
+{
+    Console.ForegroundColor = ConsoleColor.DarkGreen;
+    Console.Write("Enter a number (1 or higher): ");
+    int N = EnterNumber();
+    Console.ForegroundColor = ConsoleColor.DarkGreen;
+    Console.Write($"The cube table for the number {N}: ");
+    for (int i = 0; i <= N; i++)
+    {
+        Console.Write($"{i*i}, ");
+    }
+    Console.Write("\b \b");
+    Console.Write("\b \b");
+    Console.ResetColor();
+}
+
+int EnterNumber()
+{
+    int number = 0;
+    while(!int.TryParse(Console.ReadLine(), out number) || number < 1)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.Write("This is not a number, or this number < 1, try again: ");
+    }
+    return number;
+}
